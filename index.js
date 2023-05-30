e2d97373a5d49d17d4a15e81df4f965479199121
Sebrekov <admin@ittrue.ru>
2023-05-30 13:32:33 +0300

    start

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 import { fileURLToPath } from "node:url";
 import path from "node:path";
 import fs from 'fs';
+import { info } from "node:console";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -17,3 +18,9 @@ const content = fs.readFileSync(path.join(
 // BEGIN
 console.log(content)
 // END
+
+const dataBase = (content = 1) => {
+  const infoBase = content;
+  return infoBase;
+};
+console.log(dataBase(content));
