0960b258c0b27095f0bf816527c7cf11b12d6f2d
Sebrekov <admin@ittrue.ru>
2023-05-31 03:54:45 +0300

    start 31052023

--- index.js
+++ index.js
@@ -19,8 +19,33 @@ const content = fs.readFileSync(path.join(
 console.log(content)
 // END
 
-const dataBase = (content = 1) => {
+//Обрабатываем входные данные
+const dataBase = (contentBase) => {
+  const resultBase = [];
+  const nameHeading = parser(contentBase);
+  console.log(nameHeading);
+  //nameHeading.map((nameColumn) => resultBase.push[nameColumn]);
+  const newNameColumn = {};
+  for (const nameColumn of nameHeading) {
+    
+    newNameColumn[nameColumn] = null;
+  
+    //resultBase.push(newNameColumn);
+  }
+  const newEntity =  _.cloneDeep(newNameColumn);
+  const clearDate = 
+
+  resultBase.push(newNameColumn);
   const infoBase = content;
-  return infoBase;
+  return resultBase;
 };
+//Парсер для строки
+const parser = (stringBase) => {
+  const resultString = stringBase.split('|', 8); //Получаем массив из 1 строки 
+  resultString.shift();//Удаляем пробел в начале
+ 
+  return resultString.map((string) => string.trim());//Возвращаем массив, предварительно удалив из него лишние пробелы
+   
+};
+
 console.log(dataBase(content));
